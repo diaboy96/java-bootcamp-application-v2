810b4727e42679b91e716b53d8a64bbfdd617f3e
Martin Davidík <martin.davidik@lundegaard.eu>
2022-09-01 15:23:26 +0200

    Implementation of Lombok (Date and Organization POJO usage) in accounting-service

--- accounting-service/src/main/java/cz/martindavidik/accountingservice/dto/Date.java
+++ accounting-service/src/main/java/cz/martindavidik/accountingservice/dto/Date.java
@@ -1,12 +1,13 @@
 package cz.martindavidik.accountingservice.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
 
 import java.util.List;
 
 @JsonIgnoreProperties
+@Data
 public class Date {
-
     private String date;
     private String localName;
     private String name;
@@ -16,79 +17,4 @@ public class Date {
     private Object counties;
     private int launchYear;
     private List<String> types;
-
-    public Date() {
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getLocalName() {
-        return localName;
-    }
-
-    public void setLocalName(String localName) {
-        this.localName = localName;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCountryCode() {
-        return countryCode;
-    }
-
-    public void setCountryCode(String countryCode) {
-        this.countryCode = countryCode;
-    }
-
-    public Boolean getFixed() {
-        return fixed;
-    }
-
-    public void setFixed(Boolean fixed) {
-        this.fixed = fixed;
-    }
-
-    public Boolean getGlobal() {
-        return global;
-    }
-
-    public void setGlobal(Boolean global) {
-        this.global = global;
-    }
-
-    public Object getCounties() {
-        return counties;
-    }
-
-    public void setCounties(Object counties) {
-        this.counties = counties;
-    }
-
-    public int getLaunchYear() {
-        return launchYear;
-    }
-
-    public void setLaunchYear(int launchYear) {
-        this.launchYear = launchYear;
-    }
-
-    public List<String> getTypes() {
-        return types;
-    }
-
-    public void setTypes(List<String> types) {
-        this.types = types;
-    }
 }
