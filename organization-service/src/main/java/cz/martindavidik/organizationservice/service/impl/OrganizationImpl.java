dcdbb51b426f90867f39166c69df815de1347e40
Martin Davidík <martin.davidik@lundegaard.eu>
2022-09-01 17:13:35 +0200

    Add tipo to organization-service methods

--- organization-service/src/main/java/cz/martindavidik/organizationservice/service/impl/OrganizationImpl.java
+++ organization-service/src/main/java/cz/martindavidik/organizationservice/service/impl/OrganizationImpl.java
@@ -15,30 +15,61 @@ public class OrganizationImpl implements OrganizationService {
     @Autowired
     private OrganizationRepository organizationRepository;
 
+    /**
+     * Return all organizations
+     *
+     * @return Iterable<Organization>
+     */
     @Override
     @Transactional
     public Iterable<Organization> getAllOrganizations() {
         return organizationRepository.findAll();
     }
 
+    /**
+     * Search Organization in database by Organization name (case-insensitive)
+     *
+     * @param name - Organization´s name
+     *
+     * @return List<Organization>
+     */
     @Override
     @Transactional
     public List<Organization> findByName(String name) {
         return organizationRepository.findOrganizationsByNameContainingIgnoreCase(name);
     }
 
+    /**
+     * Search Organization in database by Organization identification number
+     *
+     * @param identificationNumber - Organization´s identification number
+     *
+     * @return List<Organization>
+     */
     @Override
     @Transactional
     public List<Organization> findByIdentificationNumber(int identificationNumber) {
         return organizationRepository.findByIdentificationNumber(identificationNumber);
     }
 
+    /**
+     * Saves Organization to database
+     *
+     * @param organization - Organization
+     *
+     * @return Organization
+     */
     @Override
     @Transactional
     public Organization save(Organization organization) {
         return organizationRepository.save(organization);
     }
 
+    /**
+     * Deletes Organization from database
+     *
+     * @param organization - Organization
+     */
     @Override
     @Transactional
     public void delete(Organization organization) {
